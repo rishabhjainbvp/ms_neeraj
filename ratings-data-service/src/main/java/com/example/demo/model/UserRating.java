e48cf7253571f8ab60927982250be2198036d156
rishabhjainbvp <rishabhjainbvp@gmail.com>
2019-07-14 14:54:43 +0200

    Add files via upload

---
+++ ratings-data-service/src/main/java/com/example/demo/model/UserRating.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class UserRating {
+
+	private List<Rating> rating;
+
+	public List<Rating> getRating() {
+		return rating;
+	}
+
+	public void setRating(List<Rating> rating) {
+		this.rating = rating;
+	}
+
+}
