49e569eec47cd1619c051495b6d5f4d434c36f2b
rishabhjainbvp <rishabhjainbvp@gmail.com>
2019-07-14 14:54:24 +0200

    Update RatingsResource.java

--- ratings-data-service/src/main/java/com/example/demo/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/demo/resources/RatingsResource.java
@@ -11,5 +11,16 @@ public class RatingsResource {
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 5);
 	}
+	
+	@RequestMapping("/user/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 3), new Rating("5678", 4));
+
+		UserRating userRating = new UserRating();
+		userRating.setRating(ratings);
+
+		return userRating;
+	}
+
 
 }
